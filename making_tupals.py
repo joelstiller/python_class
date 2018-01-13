dde320960a1fe0c5bac9ab2c148bc0ff9f0c6ffd
Joel Stiller <joelstiller@gmail.com>
2018-01-13 14:14:25 -0600

    Done with Making tupals

---
+++ making_tupals.py
@@ -0,0 +1,15 @@
+# Assignment: Dictionary in, tuples out
+# Write a function that takes in a dictionary and returns a list of tuples where the first tuple item is the key and the second is the value. Here's an example:
+
+# function input
+my_dict = {
+  "Speros": "(555) 555-5555",
+  "Michael": "(999) 999-9999",
+  "Jay": "(777) 777-7777"
+}
+
+def totupal(in_dict):
+    output = in_dict.items()
+    print output
+
+totupal(my_dict)
